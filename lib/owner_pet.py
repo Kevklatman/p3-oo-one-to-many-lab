8494006e72d1cf8e61dce28a9938a3090c342574
Kevklatman <116696816+Kevklatman@users.noreply.github.com>
2024-08-30 12:40:21 -0600

    idk

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,24 @@
+
+
 class Pet:
-    pass
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all=[]
+    def __init__(self, name, pet_type, owner=None):
+        self.name=name
+        if pet_type not in Pet.PET_TYPES:
+            raise Exception("is not pet type")
+        self.pet_type=pet_type
+        self.owner=owner
+        Pet.all.append(self)
+
 
 class Owner:
-    pass
+    def __init__(self, owner):
+        self.ownder=owner
+    def add_pet(self, pet):
+        if not isinstance(Pe):
+            raise Exception("Invalid pet. Expected an instance of Pet.")
+        self.pets.append(pet)
+        pet.owner=self
+    def pets(self):
+        return self.pets
